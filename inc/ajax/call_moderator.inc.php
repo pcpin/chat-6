d87452ae060de5025fcf14c4abd667c52e0cf689
kreznichak <pcpin@users.noreply.github.com>
2007-09-03 19:02:40 +0000

    0000216: Errors in "Call moderator" window

--- inc/ajax/call_moderator.inc.php
+++ inc/ajax/call_moderator.inc.php
@@ -46,15 +46,64 @@ if (!empty($current_user->id)) {
       // Room has no moderators. Admin(s) will receive an abuse then.
       $moderators=$current_user->getAdmins();
     }
+    _pcpin_loadClass('nickname'); $nickname=new PCPIN_Nickname($session);
     // Create message body
     $body=$current_user->id.'/'.$session->_s_room_id.'/'.($abuse_category*1).'/'.trim(str_replace('/', ' ', $abuse_nickname)).'/'.trim($abuse_description);
     foreach ($moderators as $data) {
+      _pcpin_loadClass('user'); $tmp_user=new PCPIN_User($session);
+      $tmp_user->_db_loadObj($data['id']);
       if (!empty($data['is_online'])) {
         // User is online
         $msg->addMessage(4001, 'n', $session->_s_user_id, $current_nickname, 0, $data['id'], $body, date('Y-m-d H:i:s'), 2);
-      }
-      // Add offline message
+      } else {
+        // Add offline message
 //      $msg->addMessage(4001, 'y', $session->_s_user_id, $current_nickname, 0, $data['id'], $body, date('Y-m-d H:i:s'), 2);
+      }
+      if ($session->_s_language_id!=$session->_conf_all['default_language']) {
+        $l->setLanguage($session->_conf_all['default_language']);
+      }
+      $violation_category='';
+      switch ($abuse_category) {
+
+        case 1:
+          $violation_category=$l->g('spam');
+        break;
+
+        case 2:
+          $violation_category=$l->g('insult');
+        break;
+
+        case 3:
+          $violation_category=$l->g('adult_content');
+        break;
+
+        case 4:
+          $violation_category=$l->g('illegal_content');
+        break;
+
+        case 5:
+          $violation_category=$l->g('harassment');
+        break;
+
+        case 6:
+          $violation_category=$l->g('fraud');
+        break;
+
+        case 7:
+        default:
+          $violation_category=$l->g('other');
+        break;
+
+      }
+      // Send an email
+      $email_body= $l->g('date').":\n\t".$tmp_user->makeDate(time())."\n"
+                  .$l->g('room_name').":\n\t".$current_room_name."\n"
+                  .$l->g('author').":\n\t".$nickname->coloredToPlain($current_nickname, false)."\n"
+                  .$l->g('violation_category').":\n\t".$violation_category."\n"
+                  .$l->g('abuser_nickname').":\n\t".$abuse_nickname."\n"
+                  .$l->g('violation_description').":\n\n".$abuse_description."\n"
+                  ;
+      PCPIN_Email::send('"'.$session->_conf_all['chat_email_sender_name'].'"'.' <'.$session->_conf_all['chat_email_sender_address'].'>', $tmp_user->email, $session->_conf_all['chat_name'].': '.$l->g('abuse_arrived'), null, null, $email_body);
     }
   }
 }
