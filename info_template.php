50c0a5cbfec276af46d646db1b6f993efa94429d
kreznichak <pcpin@users.noreply.github.com>
2008-01-17 17:18:49 +0000

    *** empty log message ***

---
+++ info_template.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *    This file is part of "PCPIN Chat 6".
+ *
+ *    "PCPIN Chat 6" is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    "PCPIN Chat 6" is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ Following variables are available:
++------------------------------------+------------------------------------------------------------+
+| Variable name                      | Variable description                                       |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_online_users_count         | Online users count.                                        |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_online_users               | An array with online users' names.                         |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_online_users_colored       | An array with HTML-colored online users' names.            |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_registered_users_count     | Registered users count.                                    |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_registered_users           | An array with registered users' names.                     |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_registered_users_colored   | An array with HTML-colored registered users' names.        |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_rooms_count                | Chat rooms count.                                          |
++------------------------------------+------------------------------------------------------------+
+| $_pcpin_rooms                      | Chat rooms' names.                                         |
++------------------------------------+------------------------------------------------------------+
+*/
+?>
+<div align="center">
+  <h2>This is an example page. You may edit it as you need.</h2>
+  <table border="1" cellspacing="0" cellpadding="5">
+    <tr valign="top">
+      <td>Online users count:</td>
+      <td><?php echo $_pcpin_online_users_count; ?></td>
+    </tr>
+    <tr valign="top">
+      <td>Online users:</td>
+      <td><?php echo implode(', ', $_pcpin_online_users); ?>&nbsp;</td>
+    </tr>
+    <tr valign="top">
+      <td>Online users (colored):</td>
+      <td><?php echo implode(', ', $_pcpin_online_users_colored); ?>&nbsp;</td>
+    </tr>
+    <tr valign="top">
+      <td>Registered users count:</td>
+      <td><?php echo $_pcpin_registered_users_count; ?></td>
+    </tr>
+    <tr valign="top">
+      <td>Registered users:</td>
+      <td><?php echo implode(', ', $_pcpin_registered_users); ?></td>
+    </tr>
+    <tr valign="top">
+      <td>Registered users (colored):</td>
+      <td><?php echo implode(', ', $_pcpin_registered_users_colored); ?></td>
+    </tr>
+    <tr valign="top">
+      <td>Chat rooms count:</td>
+      <td><?php echo $_pcpin_rooms_count; ?></td>
+    </tr>
+    <tr valign="top">
+      <td>Chat rooms:</td>
+      <td><?php echo implode(', ', $_pcpin_rooms); ?></td>
+    </tr>
+  </table>
+</div>
