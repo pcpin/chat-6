46384b9e449451629bd506febeedfdc360caf89e
kreznichak <pcpin@users.noreply.github.com>
2008-02-20 22:17:06 +0000

    0000336: Automatic browser language recognition does not works on some browsers

--- inc/activation.inc.php
+++ inc/activation.inc.php
@@ -25,7 +25,7 @@ if (!empty($session->_conf_all['allow_language_selection'])) {
     $preselect_language=0;
     $accept_languages=!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])? explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']) : array();
     foreach ($accept_languages as $val) {
-      $val=substr($val, 0, strpos($val, ';'));
+      $val=strpos($val, ';')? substr($val, 0, strpos($val, ';')) : $val;
       foreach ($languages as $data) {
         if (strtolower(trim($val))==$data['iso_name']) {
           $preselect_language=$data['id'];
